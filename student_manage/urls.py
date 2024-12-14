bf71ecb86f1f8cd3d46f5d09aba542466ea7f506
emuturi124@gmail.com <emuuturi124@gmail.com>
2024-12-14 17:54:21 +0300

    Implemented the search functionality of all students

--- student_manage/urls.py
+++ student_manage/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.list_student, name="home"),
     path("update/<int:id>", views.update_student, name="update"),
     path("delete/<int:id>",views.delete_student, name="delete"),
-    path("add/",views.add_student, name="add")
+    path("add/",views.add_student, name="add"),
+    path("search_student/",views.search_student, name="search")
 ]
--- student_manage/views.py
+++ student_manage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render # type: ignore
 from .models import Student
 from .forms import StudentInfoForm
-from django.http import HttpResponseRedirect # type: ignore
+from django.http import HttpResponseRedirect, HttpResponse # type: ignore
+from django.db.models import Q # type: ignore
 
 # Create your views here.
 def list_student(request):
@@ -34,3 +35,18 @@ def add_student(request):
     else:
         fm = StudentInfoForm()
     return render(request, "student_manage/add_student.html", {"form":fm})
+
+def search_student(request):
+    if request.method == "POST":
+        search=request.POST.get("output")
+        student=Student.objects.all()
+        st=None
+        if search:
+            st=student.filter(
+                Q(fname__icontains=search)|
+                 Q(lname__icontains=search)|
+                  Q(email__icontains=search)|
+                    Q(branch__icontains=search))
+        return render(request, "student_manage/list_student.html", {"st":st})
+    else:
+        return HttpResponse("An error has Occured")
